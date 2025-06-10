a942205fc50b1398f1afe6502eb5f0045b7b4f68
Trajectory-SH <trajectory0611@gmail.com>
2025-06-10 21:25:41 +0900

    [20250610] Spring Basic Start

--- spring-basic/spring-basic/core/src/test/java/hello/core/CoreApplicationTests.java
+++ spring-basic/spring-basic/core/src/test/java/hello/core/CoreApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+//spring을 띄워야 하기 때문에 조금 시간이 오래걸린다.
 class CoreApplicationTests {
 
 	@Test
