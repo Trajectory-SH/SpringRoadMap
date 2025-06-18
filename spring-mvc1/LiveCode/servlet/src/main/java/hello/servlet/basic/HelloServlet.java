9f7d6adda3d5a4aac8b3664cb798d2316533f141
Trajectory-SH <trajectory0611@gmail.com>
2025-06-18 23:44:47 +0900

    Spring Basic Component & Request Header Servlet, Hello Servlet, and Basic HTML Pages Implementation

--- spring-mvc1/LiveCode/servlet/src/main/java/hello/servlet/basic/HelloServlet.java
+++ spring-mvc1/LiveCode/servlet/src/main/java/hello/servlet/basic/HelloServlet.java
@@ -1,5 +1,31 @@
 package hello.servlet.basic;
 
-public class HelloServlet {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.IOException;
+
+// /hello로 오면 아래의 메서드가 실행된다.
+@WebServlet(name = "helloServlet",urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);//WAS server가 구현하는 http 표준 구현체
+        System.out.println("response = " + response);
+
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write("hello" + username);//HTTP message body에 내용이 들어감.
+
+    }
 }
