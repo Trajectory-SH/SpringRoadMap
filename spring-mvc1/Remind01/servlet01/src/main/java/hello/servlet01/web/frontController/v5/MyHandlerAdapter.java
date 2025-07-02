c53c3e45f2bf5ad40e778b3fced60b43e5ebe666
Trajectory-SH <trajectory0611@gmail.com>
2025-07-02 21:57:59 +0900

    MVC Pattern_V5 End

---
+++ spring-mvc1/Remind01/servlet01/src/main/java/hello/servlet01/web/frontController/v5/MyHandlerAdapter.java
@@ -0,0 +1,15 @@
+package hello.servlet01.web.frontController.v5;
+
+import hello.servlet01.web.frontController.ModelView;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public interface MyHandlerAdapter {
+    boolean supports(Object handler);
+
+    ModelView handle(HttpServletRequest request, HttpServletResponse response,Object handler)
+            throws ServletException, IOException;
+}
---
+++ spring-mvc1/Remind01/servlet01/src/main/java/hello/servlet01/web/springmvc/v3/SpringMemberControllerV3.java
@@ -0,0 +1,53 @@
+package hello.servlet01.web.springmvc.v3;
+
+
+import hello.servlet01.domain.member.Member;
+import hello.servlet01.domain.member.MemberRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+/**
+ * BestPractice -> Http API 설계
+ * Spring이 제공해주는 Model을 도입
+ * ViewName(논리) 직접 밚롼
+ *
+ * @RequestParam 사용
+ * @RequestMapping -> @GetMapping, @PostMapping
+ */
+@Controller
+@RequestMapping("/springmvc/v3/members")
+public class SpringMemberControllerV3 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @GetMapping("/new-form")
+    public String newForm() {
+        return "new-form";
+    }
+
+    @PostMapping("/save")
+    public String save(
+            @RequestParam("username") String username,
+            @RequestParam("age") int age,
+            Model model
+    ) {
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        model.addAttribute("member", member);
+
+        return "save-result";
+    }
+
+    @GetMapping
+    public String members(Model model) {
+        List<Member> members = memberRepository.findAll();
+        model.addAttribute("members", members);
+
+        return "members";
+    }
+}
