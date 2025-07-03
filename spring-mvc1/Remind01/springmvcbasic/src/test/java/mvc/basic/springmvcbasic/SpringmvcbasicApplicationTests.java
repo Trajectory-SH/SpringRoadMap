5bb4f4a31c393bc799ba8e0471dd32cfb98431ec
Trajectory-SH <trajectory0611@gmail.com>
2025-07-03 10:00:22 +0900

    .

---
+++ spring-mvc1/Remind01/springmvcbasic/src/test/java/mvc/basic/springmvcbasic/SpringmvcbasicApplicationTests.java
@@ -0,0 +1,13 @@
+package mvc.basic.springmvcbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmvcbasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
