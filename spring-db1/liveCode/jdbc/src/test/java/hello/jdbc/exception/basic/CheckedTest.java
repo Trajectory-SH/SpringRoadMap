3bbbc2a6bf1fb37fcf70ef4e54fc92b0b6d07605
Trajectory-SH <trajectory0611@gmail.com>
2025-07-26 09:50:11 +0900

    Spring DB Exception

---
+++ spring-db1/liveCode/jdbc/src/test/java/hello/jdbc/exception/basic/CheckedTest.java
@@ -0,0 +1,56 @@
+package hello.jdbc.exception.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.stereotype.Repository;
+
+@Slf4j
+public class CheckedTest {
+
+    @Test
+    void checked_catch() {
+        Service service = new Service();
+        service.callCatch();
+    }
+
+    @Test
+    void checked_throw() {
+        Service service = new Service();
+        Assertions.assertThrows(MyCheckedException.class, () -> service.callThrow());
+    }
+
+
+    static class MyCheckedException extends Exception {
+        public MyCheckedException(String message) {
+            super(message);
+        }
+    }
+
+    /**
+     * Checked 예외는 잡아서 처리 or 던지기
+     */
+    static class Service {
+        Repository repository = new Repository();
+
+        public void callCatch() {
+            try {
+                repository.call();
+            } catch (MyCheckedException e) {
+                log.info("[예외처리] message = {}", e.getMessage(), e);
+            }
+        }
+
+        public void callThrow() throws MyCheckedException {
+            repository.call();
+        }
+    }
+
+    static class Repository {
+
+        public void call() throws MyCheckedException {
+            throw new MyCheckedException("Ex");
+
+        }
+    }
+}
