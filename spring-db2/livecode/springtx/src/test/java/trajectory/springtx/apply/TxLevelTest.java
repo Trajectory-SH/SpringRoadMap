bcaa0ee178b94c28293797e32fb072eb998acf83
Trajectory-SH <trajectory0611@gmail.com>
2025-07-31 15:20:59 +0900

    Spring DB_02 Transaction

---
+++ spring-db2/livecode/springtx/src/test/java/trajectory/springtx/apply/TxLevelTest.java
@@ -0,0 +1,57 @@
+package trajectory.springtx.apply;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+@SpringBootTest
+public class TxLevelTest {
+
+    @Autowired
+    LevelService service;
+
+    @Test
+    void orderTest() {
+        service.write();
+        service.read();
+    }
+
+    @TestConfiguration
+    static class TxApplyLevelConfig {
+        @Bean
+        LevelService levelService() {
+            return new LevelService();
+        }
+    }
+
+
+    @Slf4j
+    @Transactional(readOnly = true)//읽기 전용 트랜잭션
+    static class LevelService {
+
+        @Transactional(readOnly = false)
+        public void write() {
+            log.info("call write");
+            printTxInfo();
+        }
+
+        public void read() {
+            log.info("call read");
+            printTxInfo();
+        }
+
+        private void printTxInfo() {
+            //실제로 트랜잭션이 존재하는지에 대한 여부 => 트랜잭션 동기화 매니저에 커넥션 존재 유무
+            boolean txActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("tx active = {}", txActive);
+
+            boolean readOnly = TransactionSynchronizationManager.isCurrentTransactionReadOnly();
+            log.info("tx readOnly = {}", readOnly);
+        }
+    }
+}
